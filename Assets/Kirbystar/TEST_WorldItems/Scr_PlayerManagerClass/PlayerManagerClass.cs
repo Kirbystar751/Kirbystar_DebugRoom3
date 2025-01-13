ecab7e762f7c79018675102b9e6bcd2d884733a4
Kirbystar751 <kirby_ms_winxp@outlook.jp>
2025-01-13 18:36:26 +0900

    Public変数にNonSerializedを追加

--- Assets/Kirbystar/TEST_WorldItems/Scr_PlayerManagerClass/PlayerManagerClass.cs
+++ Assets/Kirbystar/TEST_WorldItems/Scr_PlayerManagerClass/PlayerManagerClass.cs
@@ -12,64 +12,70 @@ using VRC.Udon;
 public class PlayerManagerClass : UdonSharpBehaviour
 {
     #region パブリック変数
-        /// <summary>
-        /// プレイヤー番号（内部）
-        /// VRChatSDK側で返してくる内部的なプレイヤー番号
-        /// </summary>
-        public int InternalPlayerIndex;
-        /// <summary>
-        /// プレイヤー番号（通常）
-        /// プログラムで割り付ける、独自のプレイヤー番号
-        /// 空いてる若い枠から順番に割り当てていく
-        /// </summary>
-        public int PlayerIndex;
-        /// <summary>
-        /// プレイヤーの名前
-        /// </summary>
-        public string PlayerName;
-        /// <summary>
-        /// VRモードかどうか
-        /// </summary>
-        public bool IsUseVR;
-        /// <summary>
-        /// プレイヤーのプラットフォーム
-        /// </summary>
-        public int Platform;
-        /// <summary>
-        /// プレイヤーのデバイスタイプ
-        /// </summary>
-        public int DeviceType;
+    /// <summary>
+    /// プレイヤー番号（内部）
+    /// VRChatSDK側で返してくる内部的なプレイヤー番号
+    /// </summary>
+    [NonSerialized]
+    public int InternalPlayerIndex;
+    /// <summary>
+    /// プレイヤー番号（通常）
+    /// プログラムで割り付ける、独自のプレイヤー番号
+    /// 空いてる若い枠から順番に割り当てていく
+    /// </summary>
+    [NonSerialized]
+    public int PlayerIndex;
+    /// <summary>
+    /// プレイヤーの名前
+    /// </summary>
+    [NonSerialized]
+    public string PlayerName;
+    /// <summary>
+    /// VRモードかどうか
+    /// </summary>
+    [NonSerialized]
+    public bool IsUseVR;
+    /// <summary>
+    /// プレイヤーのプラットフォーム
+    /// </summary>
+    [NonSerialized]
+    public int Platform;
+    /// <summary>
+    /// プレイヤーのデバイスタイプ
+    /// </summary>
+    [NonSerialized]
+    public int DeviceType;
     #endregion
     #region 定数
-        /// <summary>
-        /// プレイヤーのプラットフォームを表す値
-        /// </summary>
-        const int PLAYER_PLATFORM_PC = 0;
-        const int PLAYER_PLATFORM_ANDROID = 1;
-        const int PLAYER_PLATFORM_UNKNOWN = 255;
-        /// <summary>
-        /// プレイヤーのデバイス種別を表す値
-        /// </summary>
-        /// <summary>
-        /// PCデスクトップ
-        /// </summary>
-        const int PLAYER_DEVICE_PC_DESKTOP = 0;
-        /// <summary>
-        /// PC VR
-        /// </summary>
-        const int PLAYER_DEVICE_PCVR = 1;
-        /// <summary>
-        /// Androidスマホ
-        /// </summary>
-        const int PLAYER_DEVICE_ANDROID_MOBILE = 2;
-        /// <summary>
-        /// Quest単機
-        /// </summary>
-        const int PLAYER_DEVICE_QUEST = 3;
-        /// <summary>
-        /// その他
-        /// </summary>
-        const int PLAYER_DEVICE_UNKNOWN = 255;
+    /// <summary>
+    /// プレイヤーのプラットフォームを表す値
+    /// </summary>
+    const int PLAYER_PLATFORM_PC = 0;
+    const int PLAYER_PLATFORM_ANDROID = 1;
+    const int PLAYER_PLATFORM_UNKNOWN = 255;
+    /// <summary>
+    /// プレイヤーのデバイス種別を表す値
+    /// </summary>
+    /// <summary>
+    /// PCデスクトップ
+    /// </summary>
+    const int PLAYER_DEVICE_PC_DESKTOP = 0;
+    /// <summary>
+    /// PC VR
+    /// </summary>
+    const int PLAYER_DEVICE_PCVR = 1;
+    /// <summary>
+    /// Androidスマホ
+    /// </summary>
+    const int PLAYER_DEVICE_ANDROID_MOBILE = 2;
+    /// <summary>
+    /// Quest単機
+    /// </summary>
+    const int PLAYER_DEVICE_QUEST = 3;
+    /// <summary>
+    /// その他
+    /// </summary>
+    const int PLAYER_DEVICE_UNKNOWN = 255;
 
     const string PLAYERDATA_KEY_PLAYER_DATA = "PlayerData";
     const string PLAYERDATA_KEY_PLAYER_DATA_INTERNAL_IDX = "PlayerData_Internal";
