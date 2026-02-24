79e213ea9a1414228ddb55b6161c8ef63eb78749
Kirbystar751 <kirby_ms_winxp@outlook.jp>
2026-02-24 20:30:23 +0900

    [SGB_ColorSim]後から来た人がそれまでいた人のデータをかき消す問題を解消？ #5

--- Assets/Kirbystar/SGB_ColorSim/Script/SGB_ColorSim_Core.cs
+++ Assets/Kirbystar/SGB_ColorSim/Script/SGB_ColorSim_Core.cs
@@ -692,8 +692,8 @@ public class SGB_ColorSim_Core : UdonSharpBehaviour
 
     void Start()
     {
-        SyncManager.syncKind = SGB_ColorSim_SyncManager.SYNC_KIND_PASSWORD;
-        SyncManager.SetPassword(SGBPassword);
+        //SyncManager.syncKind = SGB_ColorSim_SyncManager.SYNC_KIND_PASSWORD;
+        //SyncManager.SetPassword(SGBPassword);
     }
 
     public void ev_Pass2Color()
