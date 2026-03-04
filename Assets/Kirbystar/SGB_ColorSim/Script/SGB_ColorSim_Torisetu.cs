eb2e035be0cf4c7861e65dd8709b348081cc0d3c
Kirbystar751 <kirby_ms_winxp@outlook.jp>
2026-03-04 19:58:12 +0900

    [SGB_ColorSim]取説

--- Assets/Kirbystar/SGB_ColorSim/Script/SGB_ColorSim_Torisetu.cs
+++ Assets/Kirbystar/SGB_ColorSim/Script/SGB_ColorSim_Torisetu.cs
@@ -6,8 +6,18 @@ using VRC.Udon;
 
 public class SGB_ColorSim_Torisetu : UdonSharpBehaviour
 {
+
+    int pageIndex = 0;
+    Animator torisetuAnim;
     void Start()
     {
-        
+        torisetuAnim = GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        pageIndex++;
+        if (pageIndex > 3) pageIndex = 0;
+        torisetuAnim.SetInteger("Page",pageIndex);
     }
 }
