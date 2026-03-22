1a895b9e561621e753bda55468c173838c061b2a
Kirbystar751 <kirby_ms_winxp@outlook.jp>
2026-03-22 17:30:11 +0900

    [SGB_ColorSim]イラスト変更を追加

---
+++ Assets/Kirbystar/SGB_ColorSim/Script/SGB_ColorSim_IllustButton.cs
@@ -0,0 +1,25 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SGB_ColorSim_IllustButton : UdonSharpBehaviour
+{
+    [SerializeField] public Texture[] Illusts;
+    [SerializeField] public MeshRenderer[] IllustMats = new MeshRenderer[4];
+    AudioSource Sound;
+    [SerializeField]public AudioClip IllustChangeSound;
+
+    void Start()
+    {
+        Sound = GetComponent<AudioSource>();
+    }
+
+    public override void Interact()
+    {
+        Sound.Stop();
+        Sound.PlayOneShot(IllustChangeSound);
+    }
+}
